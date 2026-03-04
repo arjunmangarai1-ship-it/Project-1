cbd09b79335b7bc588efa34f0be5f6ed93af1a7e
arjunmangarai1-ship-it <arjunmangarai1@gmail.com>
2026-03-04 12:41:33 -0500

    Add files via upload

---
+++ datatype.py
@@ -0,0 +1,28 @@
+
+age = 25                 
+salary = 55000.75        
+complex_num = 2 + 3j      
+name = "Arjun"           
+is_employee = True        
+skills = ["Python", "SQL", "Supply Chain"]
+experience_years = (2016, 2025)
+certifications = {"Salesforce", "AWS", "Python"}
+employee_details = {
+    "name": name,
+    "age": age,
+    "salary": salary,
+    "skills": skills
+}
+manager = None
+
+
+print("my Name:", name)
+print("Age:", age)
+print("Salary:", salary)
+print("Complex Number:", complex_num)
+print("Is Employee:", is_employee)
+print("Skills:", skills)
+print("Experience Years:", experience_years)
+print("Certifications:", certifications)
+print("Employee Details:", employee_details)
+print("Manager:", manager)
